f26554b8c022b32b12d5a658fb7e543a09de93cb
Joaoferraz-byte <2.00160983e+08+Joaoferraz-byte@users.noreply.github.com>
2026-07-28 13:47:25 +0000

    Update dashboard config path and initial structure

--- lua/ui/init.lua
+++ lua/ui/init.lua
@@ -14,7 +14,7 @@ require("dashboard").setup({
       { icon = "  ", desc = "Novo Arquivo", action = "enew", key = "n" },
       { icon = "  ", desc = "Arquivos Recentes", action = "Telescope oldfiles", key = "r" },
       { icon = "  ", desc = "Explorar Arquivos", action = "NvimTreeToggle", key = "e" },
-      { icon = "  ", desc = "Configuração", action = "e ~/.config/nvim/init.lua", key = "c" },
+      { icon = "  ", desc = "Configuração", action = "e ~/Projects/lua-conf/init.lua", key = "c" },
       { icon = "  ", desc = "Sair do Neovim", action = "qa", key = "q" },
     },
   },
